8efd51487be8e238c7a656c5e53c40067f9e08b1
Lauren21717 <laurenyeung217@gmail.com>
2025-06-04 14:23:09 +0100

    Completed search article by id endpoints and testing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { getApi } = require('./controllers/api.controllers');
 const { getTopics } = require('./controllers/topics.controllers');
-const { getArticles } = require('./controllers/articles.controllers');
+const { getArticles, getArticleById } = require('./controllers/articles.controllers');
 const { getUsers } = require('./controllers/users.controllers');
 
 const app = express();
@@ -12,8 +12,22 @@ app.use(express.json());
 app.get('/api', getApi);
 app.get('/api/topics', getTopics);
 app.get('/api/articles', getArticles);
+app.get('/api/articles/:article_id', getArticleById);
 app.get('/api/users', getUsers);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({ msg: err.msg });
+    } else if (err.code === '22P02') {
+        res.status(400).send({ msg: 'Bad request' });
+    } else {
+        next(err);
+    }
+});
 
+app.use((err, req, res, next) => {
+    res.status(500).send({ msg: 'Internal server error' });
+});
 
 module.exports = app;
--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,9 +1,19 @@
-const { selectArticles } = require("../models/articles.models")
+const { selectArticles, selectArticleById } = require("../models/articles.models")
 
 exports.getArticles = (req, res, next) => {
     selectArticles()
         .then((articles) => {
             res.status(200).send({ articles });
+        })
+        .catch(next);
+};
+
+exports.getArticleById = (req, res, next) => {
+    const { article_id } = req.params;
+    
+    selectArticleById(article_id)
+        .then((article) => {
+            res.status(200).send({ article });
         })
         .catch(next);
 };
