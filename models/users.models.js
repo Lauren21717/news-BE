0f217bffaeff06a6abba438e1f780e046890ddd5
Lauren21717 <laurenyeung217@gmail.com>
2025-06-05 17:27:26 +0100

    Add get username

--- models/users.models.js
+++ models/users.models.js
@@ -6,3 +6,21 @@ exports.selectUsers = () => {
             return rows;
         });
 };
+
+exports.selectUserByUsername = (username) => {
+    if (!username || typeof username !== 'string') {
+        return Promise.reject({ status: 400, msg: 'Bad request' });
+    }
+
+    return db.query(`
+        SELECT username, name, avatar_url
+        FROM users 
+        WHERE username = $1;
+        `, [username])
+        .then(({rows}) => {
+            if (rows.length === 0) {
+                return Promise.reject({ status: 404, msg: 'User not found' });
+            }
+            return rows[0];
+        });
+};
