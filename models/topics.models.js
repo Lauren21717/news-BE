54b2dfaac1c20d6bdf47e49d5ed965beb4d6c9e1
Lauren21717 <laurenyeung217@gmail.com>
2025-06-06 17:02:05 +0100

    Refactor topics models

--- models/topics.models.js
+++ models/topics.models.js
@@ -1,25 +1,22 @@
 const db = require("../db/connection");
 
-exports.selectTopics = () => {
-    return db.query(`SELECT * FROM topics;`)
-        .then(({ rows }) => {
-            return rows;
-        });
+exports.selectTopics = async () => {
+    const { rows } = await db.query(`SELECT * FROM topics;`);
+    return rows;
 };
 
-exports.insertTopic = (slug, description) => {
+exports.insertTopic = async (slug, description) => {
     if (!slug || !description || slug.trim() === '') {
         return Promise.reject({ status: 400, msg: 'Bad request' });
     }
 
     const defaultImgUrl = 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700';
 
-    return db.query(`
+    const { rows } = await db.query(`
       INSERT INTO topics (slug, description, img_url)
       VALUES ($1, $2, $3)
       RETURNING *;
-    `, [slug.trim(), description, defaultImgUrl])
-        .then(({ rows }) => {
-            return rows[0];
-        });
+    `, [slug.trim(), description, defaultImgUrl]);
+
+    return rows[0];
 };
