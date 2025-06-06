b7b7fbb98193b5338f793e505b2ac376536c1c8a
Lauren21717 <laurenyeung217@gmail.com>
2025-06-06 17:03:20 +0100

    Refactor users models

--- models/users.models.js
+++ models/users.models.js
@@ -1,11 +1,9 @@
 const db = require("../db/connection");
 const { selectFollowedTopicsByUsername } = require("./user-topics.models");
 
-exports.selectUsers = () => {
-    return db.query(`SELECT username, name, avatar_url FROM users;`)
-        .then(({ rows }) => {
-            return rows;
-        });
+exports.selectUsers = async () => {
+    const { rows } = await db.query(`SELECT username, name, avatar_url FROM users;`)
+    return rows;
 };
 
 exports.selectUserByUsername = async (username) => {
